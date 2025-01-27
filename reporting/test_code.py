49ecaad7a195ecd37286aaa9dee632cb4bbfb4b3
Elaine French <elainefrench@Elaines-MacBook-Pro.local>
2025-01-27 11:44:55 -0700

    updated test_code file and created gitignore file

--- reporting/test_code.py
+++ reporting/test_code.py
@@ -1,11 +1,33 @@
 import pandas as pd
+from pathlib import Path
 
-def get_file_path(folder):
-    pass
+class DataLoader():
+    def __init__(self,folder_path):
+        self.folder = Path(folder_path)
+        self.file_path = self.get_file_path()
 
-def upload_file(file_path,type="csv"):
-    if(type=="xlsx"):
-        df = pd.read_excel(file_path)
-    else:
-        df = pd.read_csv(file_path)
-    return df
+    def get_file_path(self):
+        files_list = [f for f in self.folder.iterdir() if f.is_file()]
+        if not files_list:
+            raise FileNotFoundError(f"No files found in folder")
+        
+        file_name = files_list[0].name
+        file_path = self.folder / file_name
+        return str(file_path)
+
+    def upload_file(self,type="csv"):
+        if(type=="xlsx"):
+            df = pd.read_excel(self.file_path)
+        else:
+            df = pd.read_csv(self.file_path)
+
+        drop_cols = []
+        for col in df.columns:
+            if "Unnamed" in col:
+                drop_cols.append(col)
+
+        if drop_cols:
+            df.drop(columns=drop_cols,inplace=True)
+            
+        return df
+
